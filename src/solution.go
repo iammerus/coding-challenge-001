556d707441872f2f6f922753aae054586bb55e5f
Melvin Mupondori <melmups@outlook.com>
2020-04-09 07:18:58 +0200

    Finalized solution

--- src/solution.go
+++ src/solution.go
@@ -1,9 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	fmt.Println(reverse("A4B5C2"))
 }
 
 func reverse(input string) string {
-	return ""
+	// Use a regular expression to extract all combinations of letters and numbers
+	r := regexp.MustCompile("(?P<Letter>[A-z])(?P<Count>\\d)")
+
+	// Find all matches for the regular expression in the input string
+	match := r.FindAllStringSubmatch(input, -1)
+
+	output := ""
+
+	// Interate over the combinations, repeating the letters and adding that to output string
+	for _, element := range match {
+		count, _ := strconv.Atoi(element[2])
+
+		output += strings.Repeat(element[1], count)
+	}
+
+	return output
 }
